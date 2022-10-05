b8db2dab389e7ab7825db74fe48cb503a7748a01
NgoVTruong <ngovtruong000@gmail.com>
2022-10-06 01:24:00 +0700

    application that returns prime numbersfor a range

--- csharp fundamental day3/Program.cs
+++ csharp fundamental day3/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace csharp_fundamental_day3
+{
+    class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            var watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+
+            var oneSecond = OneSecond();
+            var twoSecond = TwoSecond();
+            var threeSecond = ThreeSecond();
+
+            var results = await Task.WhenAll(oneSecond, twoSecond, threeSecond);
+
+            Console.WriteLine($"Random number from 0 to 100: {results[0]}");
+            Console.WriteLine($"Random number from 0 to 100: {results[1]}");
+            Console.WriteLine($"Random number from 0 to 100: {results[2]}");
+
+            Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
+
+            Console.ReadLine();
+        }
+
+        private static async Task<int> OneSecond()
+        {
+            Random random = new Random();
+            int randomNumb = random.Next(0, 100);
+            await Task.Delay(1000);
+            return randomNumb;
+        }
+
+        private static async Task<int> TwoSecond()
+        {
+            Random random = new Random();
+            int randomNumb = random.Next(0, 100);
+            await Task.Delay(2000);
+            return randomNumb;
+        }
+        private static async Task<int> ThreeSecond()
+        {
+            Random random = new Random();
+            int randomNumb = random.Next(0, 100);
+            await Task.Delay(3000);
+            return randomNumb;
+        }
+    }
+}
